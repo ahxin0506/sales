6febfe71cd6c14e13c86fd96048a600d689184f5
ahxin0506 <tiongingxin0506@gmail.com>
2025-07-04 15:43:08 +0800

    Update cart2.php

--- cart2.php
+++ cart2.php
@@ -33,7 +33,7 @@ body,td,th {
      echo "</tr>";
      echo "</table><BR>";	
 	 echo"<td>成功加入購物車 !</td>";
-     echo'<a href="http://120.107.152.110/~a0418/HA/newproduct.html">回首頁</a><br>';
+     echo'<a href="newproduct.html">回首頁</a><br>';
 	
 	mysqli_free_result($result);
     mysqli_close($link); 
